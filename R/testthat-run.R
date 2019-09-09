8696abc688bc5c00cea59e5f019bdb5f119f2582
James Balamuta <balamut2@illinois.edu>
2019-09-08 21:12:46 -0500

    Add the main runner.

---
+++ R/testthat-run.R
@@ -0,0 +1,48 @@
+#' Automatically Test a Set of Unit Tests in a Directory
+#' 
+#' Parses a set of files found in the `testthat` directory inside a docker
+#' container.
+#' 
+#' @param dir        Directory where unit tests are written
+#' 
+#' @return 
+#' 
+#' A `list` with a format that matches the external grading specification. 
+#' See the details section for more information.
+#' 
+#' @section Directory Testing:
+#' By default, `testthat` has a preference to run tests within an _R_ package.
+#' This approach uncouples the need for an _R_ package. Details for testing
+#' outside of an _R_ package can be found at:
+#' <https://github.com/r-lib/testthat/issues/659>
+#' 
+#' @details 
+#' 
+#' TODO: Add details or link out.
+#' <https://prairielearn.readthedocs.io/en/latest/externalGrading/#grading-results>
+#' 
+#' @export
+run_testthat = function(dir = "testthat") {
+  
+  # Begin the process of grading each student's code
+  # Add a capture statement to avoid allowing students to output
+  # content to console.
+  log <- utils::capture.output({
+    test_data <- as.data.frame(testthat::test_dir("testthat",
+                              reporter = CustomListReporter))
+  })
+  
+  test_results = format_testthat_to_pl(test_data)
+  
+  # Output is manipulated from the run.sh file into results.json
+  jsonlite::write_json(
+    test_results,         # Pre-formatted data in a data.frame
+    path = stdout(),      # Dump into Standard Out (STDOUT) to avoid tracking files.
+    pretty = TRUE,        # Make the JSON legible
+    auto_unbox = TRUE,    # Remove `[]` around single element in a vector
+    force = TRUE,         # Avoid unclassing any item without a direct map
+    always_decimal = TRUE # Required due to 0's not being coded as 0.0
+  )
+  
+  invisible(test_results)
+}
