00c5f0cd7de308e9e5675d70d2fb3602e2f36337
James J Balamuta <balamut2@illinois.edu>
2019-11-20 14:37:53 -0600

    Path changes.

--- R/path-helpers.R
+++ R/path-helpers.R
@@ -4,24 +4,26 @@
 #' 
 #' @param file_name  Name of the file to construct the file path. 
 #' @param fixed_path Create an absolute (e.g. root `/` path) or relative. 
-#'                   Default is a relative path. 
+#'                   Default is an absolute path. 
 #' 
 #' @export
 #' @rdname path_helper
-path_student = function(file_name, fixed_path = FALSE) {
+path_student = function(file_name, fixed_path = TRUE) {
   if(fixed_path) {
     file.path("", "grade", "student", file_name)
   } else {
-    file.path("..", file_name)
+    # Assumes working directory is /grade/run/testthat
+    file.path("..", "..", "student", file_name)
   }
 }
 
 #' @export
 #' @rdname path_helper
-path_answer = function(file_name, fixed_path = FALSE) {
+path_answer = function(file_name, fixed_path = TRUE) {
   if(fixed_path) {
     file.path("", "grade", "tests", "testthat", file_name)
   } else {
+    # Assumes working directory is /grade/run/testthat
     file.path(file_name)
   }
 }
